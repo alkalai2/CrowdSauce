02afc231b53852475af6caf0083399f96fd2a060
CharlieMartell <charliecmartell@gmail.com>
2016-02-13 04:33:43 -0600

    add basic backend

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+// Setup boilerplate code for server
+// Async needed for realtime responses, think AJAX
+var async = require('async');
+// Express handles incoming requests
+var express = require('express');
+// RethinkDB handles data storage
+var rethinkdb = require('rethinkdb');
+
+// Config file containing server and port information
+var config = require(__dirname + '/config.js');
+
+
+// TODO: Setup everything else
+
