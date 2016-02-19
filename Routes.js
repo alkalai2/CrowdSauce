949f9a15e8a38cb4d1b18f0b53c3e74d8cbf346d
CharlieMartell <charliecmartell@gmail.com>
2016-02-19 15:06:16 -0600

    Refactor Routes.js for userid instead of username

--- Routes.js
+++ Routes.js
@@ -1,10 +1,10 @@
 // Note prefix all with /api/
 function setup (app, handlers) {
-  app.post('/api/users', handlers.account.createAccount)
+  app.post('/api/users/:username', handlers.account.createAccount)
   app.get('/api/users/:username', handlers.account.getAccount)
   app.put('/api/users/:username', handlers.account.updateAccount)
   app.delete('/api/users/:username', handlers.account.deleteAccount)
-  app.post('/api/posts/username', handlers.post.createPost)
+  app.post('/api/posts/:username', handlers.post.createPost)
   app.get('/api/posts/:username', handlers.post.getPost)
   app.put('/api/posts/:username', handlers.post.updatePost)
   app.delete('/api/posts/:username', handlers.post.deletePost)
