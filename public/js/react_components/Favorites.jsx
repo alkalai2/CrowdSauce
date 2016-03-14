7b705c9818201f81faeedb49b7ed8181aa9f1940
alkalai2 <jonroks94@gmail.com>
2016-03-13 19:20:27 -0500

    add Favorites page post loading

--- public/js/react_components/Favorites.jsx
+++ public/js/react_components/Favorites.jsx
@@ -14,15 +14,15 @@ var Favorites = React.createClass({
       var self = this
       getFacebookDetails().then(function(fbDetails) {
         console.log("Getting facebook details : " + fbDetails)
-        self.loadPostsFromServer(fbDetails)
+        self.loadPostIdsFromServer(fbDetails)
       }, function(error) {
         console.log("Error : " + error)
       })
     },
 
-    loadPostsFromServer : function(fbDetails) {
+    loadPostIdsFromServer : function(fbDetails) {
 
-        console.log("getting posts from server..."); 
+        console.log("getting favorites from server..."); 
         jQuery.ajax({
           url:  this.props.source,
           type: 'GET',
@@ -34,15 +34,54 @@ var Favorites = React.createClass({
           dataType: 'json',
           timeout : 10000,
           success: function(data) {
-            console.log("setting state with data ... ")
             console.log(data)
-            this.setState({data: data});
+            this.loadFullPostsFromServer(data, fbDetails)
           }.bind(this),
           error: function(xhr, status, err) {
             console.error(this.props.source, status, err.toString());
           }.bind(this)
         }); 
     },
+
+    loadFullPostsFromServer: function(data, fbDetails) {
+      
+      // using the postIds, load the full JSON data for each post
+      posts_data = []  
+      console.log("getting favorite posts from ids ...")
+      for(d in data) {
+
+          postId = data[d]['postId']
+          console.log("postId to be called with : " + postId)
+          var url = "http://localhost:3000/api/posts/"
+          jQuery.ajax({
+            url:  url,
+            type: 'GET',
+            headers: {
+              'Accept': 'text/html',
+              'userid': fbDetails['fbUserID'],
+              'accesstoken': fbDetails['fbAccessToken'],
+            },
+            data : {
+              'postId': postId
+            },
+            dataType: 'json',
+            timeout : 10000,
+            success: function(data) {
+              console.log(data)
+              posts_data = posts_data.concat(data)
+              this.setState({data: posts_data})
+            }.bind(this),
+            error: function(xhr, status, err) {
+              console.error(status, err.toString());
+            }.bind(this)
+          });
+        }
+
+      // console.log("favorites full post data : ")
+      // console.log(posts_data)
+      // // bind new post data to class
+      // this.setState({data: posts_data})
+    }, 
 
     render: function() {
    		return (
