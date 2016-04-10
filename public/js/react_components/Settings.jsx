fd330ef70f3ea6300f3221c557f0f2b9979d0529
Charlie Anderson <charlieanderson13@gmail.com>
2016-04-10 12:51:45 -0500

    improved settings page ui

--- public/js/react_components/Settings.jsx
+++ public/js/react_components/Settings.jsx
@@ -1,34 +1,82 @@
 
 
-
+var Input = ReactBootstrap.Input
+var Button = ReactBootstrap.Button
+var Image = ReactBootstrap.Image
+var Thumbnail = ReactBootstrap.Thumbnail
 var Settings = React.createClass ({
+
+	getInitialState() {
+		return{
+			value: '',
+			imgsrc: 'https://s-media-cache-ak0.pinimg.com/736x/1a/39/55/1a39555080409fad4d86b94a9af46b97.jpg',
+		};
+	},
+
+  	changeImgSrc: function(e) {
+      this.setState({imgsrc: e.target.value});
+  	},
+
+	handleEmailChange() {
+		this.setState({
+			value:this.refs.input.getValue()
+		});
+	},
+
     render: function() {
+
+    	var imgStyle = {
+    		float: 'right',
+		    display: 'inline-block'
+    	}
     	return (
+
     		<div>
-            <span className="setting_label">Setting 1</span> 
+		      <Thumbnail className="settingsThumb" src={this.state.imgsrc} alt="Enter a valid Picture link">
 
-          <label className="switch">
-            <input className="switch-input" type="checkbox" />
-            <span className="switch-label" data-on="On" data-off="Off"></span>
-            <span className="switch-handle"></span> 
-          </label>
+			  <Input
+			    type="text"
+			    label="Change Profile Picture"
+			    placeholder="Image URL"
+			    wrapperClassName="col-xs-15"
+			    onChange={this.changeImgSrc}
+			    value={this.state.imgsrc}
+			  />
+	          <Input 
+				type="text"
+				value={this.state.value}
+				placeholder="Enter new email"
+				label="Change Email Address"
+				onChange={this.handleEmailChange}
+				ref="input"
+        		groupClassName="group-class"
+        		labelClassName="label-class"
+	          />
+	          <Input 
+				type="text"
+				value={this.state.value}
+				placeholder="Enter new Username"
+				label="Change Username"
+				onChange={this.handleEmailChange}
+				ref="input"
+        		groupClassName="group-class"
+        		labelClassName="label-class"
+	          />
 
-            <span className="setting_label">Setting 2</span> 
 
-          <label className="switch"> 
-            <input className="switch-input" type="checkbox" />
-            <span className="switch-label" data-on="On" data-off="Off"></span>
-            <span className="switch-handle"></span> 
-          </label>
+		      <span className="setting_label">Allow email notifications?</span> 
+	          <label className="switch">
+	            <input className="switch-input" type="checkbox"/>
+	            <span className="switch-label" data-on="On" data-off="Off"></span>
+	            <span className="switch-handle"></span> 
+	          </label>
 
-            <span className="setting_label">Setting 3</span> 
+		      </Thumbnail>
+         	<p>
+	          <Button bsStyle="primary">Save</Button>&nbsp;
+	        </p>
 
-          <label className="switch"> 
-            <input className="switch-input" type="checkbox" />
-            <span className="switch-label" data-on="On" data-off="Off"></span>
-            <span className="switch-handle"></span> 
-          </label>
-        </div>
+        	</div>
     		);
     },
 })
