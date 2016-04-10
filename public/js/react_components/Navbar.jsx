6d8b103191fb45ad81d47dbb0aefad76060b3efe
Charlie Anderson <charlieanderson13@gmail.com>
2016-04-10 14:08:28 -0500

    refactored the navbar to use react, not html

---
+++ public/js/react_components/Navbar.jsx
@@ -0,0 +1,32 @@
+
+var Nav = ReactBootstrap.Navbar
+
+var Navbar = React.createClass ({
+
+  render:function(){
+    return(
+      <Nav role="navigation" fixedTop="true" inverse="true" fluid="true">
+        <a className="navbar-brand" href="/">CrowdSauce</a>
+        <ul className="nav navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link" href="/feed">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/about">About</a>
+          </li>
+          <li className="nav-item ">
+            <a className="nav-link" href="/favorites">Favorites</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/profile">My Profile</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/settings">Settings</a>
+          </li>
+        </ul>
+      </Nav>
+    );
+  },
+});
+
+ReactDOM.render(<Navbar/>, navbar);
