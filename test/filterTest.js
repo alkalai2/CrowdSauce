476e03d78fdd455fb6d1e226edb33955d2254f76
LUKK3 <lupuchard@gmail.com>
2016-04-23 21:40:02 -0500

    Filter test

---
+++ test/filterTest.js
@@ -0,0 +1,114 @@
+var assert = require('assert')
+request = require('request')
+app = require('../app')
+
+describe('filter tests', function () {
+  function createAccounts(accounts, callback) {
+    if (accounts.length == 0) {
+      callback()
+      return
+    }
+    request.post({
+      url: "http://localhost:3000/api/accounts/",
+      headers: {'userid': accounts[accounts.length - 1]}
+    }, function (err, res, body) {
+      //assert.equal(200, res.statusCode)
+      accounts.pop()
+      createAccounts(accounts, callback)
+    })
+  }
+
+  function addTags(post, tags, callback) {
+    if (tags.length == 0) {
+      callback()
+      return
+    }
+    bod = '{"tagName": "' + tags[tags.length - 1] + '", "postId": "' + post + '"}'
+    request.post({
+      url: "http://localhost:3000/api/tags/",
+      headers: {'Content-Type': 'application/json'},
+      body: '{"tagName": "' + tags[tags.length - 1] + '", "postId": "' + post + '"}'
+    }, function (err, res, body) {
+      assert.equal(200, res.statusCode)
+      tags.pop()
+      addTags(post, tags, callback)
+    })
+  }
+
+  function addFavorites(post, favorites, callback) {
+    if (favorites.length == 0) {
+      callback()
+      return
+    }
+    request.post({
+      url: "http://localhost:3000/api/favorites/",
+      headers: {
+        'Content-Type': 'application/json',
+        'userid': favorites[favorites.length - 1]
+      },
+      body: '{"postId": "' + post + '"}'
+    }, function (err, res, body) {
+      assert.equal(200, res.statusCode)
+      favorites.pop()
+      addFavorites(post, favorites, callback)
+    })
+  }
+
+  function createPosts(account, titles, tags, ratings, favorites, callback) {
+    if (titles.length == 0) {
+      callback()
+      return
+    }
+    request.post({
+      url: "http://localhost:3000/api/posts/",
+      headers: {'userid': account},
+      body: '{"title": "' + titles[titles.length - 1] + '", "rating": ' + ratings[ratings.length - 1] + '}'
+    }, function (err, res, body) {
+      body = JSON.parse(body)
+      assert.equal(200, res.statusCode)
+      addTags(body["postId"], tags[tags.length - 1], function() {
+        addFavorites(body["postId"], favorites[favorites.length - 1], function() {
+          titles.pop()
+          tags.pop()
+          ratings.pop()
+          favorites.pop()
+          createPosts(account, titles, tags, ratings, favorites, callback)
+        })
+      })
+    })
+  }
+
+  function assertFeed(account, tags, feed, callback) {
+    request.get({
+      url: "http://localhost:3000/api/tags/feed/?tagNames=" + tags,
+      headers: {'userid': account}
+    }, function (err, res, body) {
+      assert.equal(200, res.statusCode)
+      callback(JSON.parse(res.body))
+    })
+  }
+
+  var adam = 100463527019300
+  var brad = 107662552963871
+  var greg = 153655315025677
+  var dave = 104466886616471
+  it('filter stuff', function (done) {
+    process.env.CRS_DEBUG = 1
+    createAccounts([adam, brad, greg, dave], function () {
+      createPosts(
+        adam,
+        ['a', 'b', 'c', 'd', 'e'],
+        [['ham'], ['soup'], ['soup', 'ham'], ['soup'], ['ham']],
+        [1, 5, 3, 4, 1],
+        [[brad, greg], [brad], [], [brad], [brad, greg, dave]],
+        function () {
+          assertFeed(adam, "rating:3", ['b', 'c', 'd'], function() {
+            assertFeed(adam, "sort:favorites, soup", ['b', 'd', 'c'], function() {
+              console.log("SUCCEED")
+            })
+          })
+        }
+      )
+    })
+  })
+})
